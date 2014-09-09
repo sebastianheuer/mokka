5ed0dcef0ad33dd876ce862931bc88739b481b5f
Sebastian Heuer <sebastian.heuer@globetrotter.de>
2014-09-09 12:49:05 +0200

    added simple PHPUnit testcase

---
+++ src/framework/phpunit/PHPUnitMokkaTestCase.php
@@ -0,0 +1,26 @@
+<?php
+namespace Mokka\PHPUnit;
+
+use Mokka\Mock\MockInterface;
+use Mokka\Mokka;
+
+class MokkaTestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param string $classname
+     * @return \Mokka\Mock\Mock
+     */
+    public function mock($classname)
+    {
+        return Mokka::mock($classname);
+    }
+
+    /**
+     * @param MockInterface $mock
+     * @return MockInterface
+     */
+    public function when(MockInterface $mock)
+    {
+        return Mokka::when($mock);
+    }
+} 
